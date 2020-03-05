c5d6e4ce0aa703f4266f8d023c117a147d9c631a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-05 12:20:20 -0700

    Fix zero-as-null-pointer-constant warning for time
    Reference:
    https://en.cppreference.com/w/cpp/chrono/c/time

--- server/GameServer.cpp
+++ server/GameServer.cpp
@@ -263,7 +263,7 @@ void GameServer::ProcessGameSearchReply(Packet& packet, sockaddr_in& from)
 	gameSession.flags |= GameSessionReceived;
 	gameSession.flags &= ~GameSessionExpected & ~GameSessionUpdateRetrySent;
 	gameSession.createGameInfo = packet.tlMessage.searchReply.createGameInfo;
-	gameSession.time = time(0);
+	gameSession.time = time(nullptr);
 }
 
 void GameServer::ProcessPoke(Packet& packet, sockaddr_in& from)
@@ -299,7 +299,7 @@ void GameServer::ProcessPoke(Packet& packet, sockaddr_in& from)
 		newGameSession.clientRandValue = packet.tlMessage.gameServerPoke.randValue;
 		newGameSession.serverRandValue = GetNewRandValue();
 		newGameSession.flags |= GameSessionExpected;
-		newGameSession.time = time(0);
+		newGameSession.time = time(nullptr);
 
 		// Send a request for games
 		SendGameSessionRequest(from, newGameSession.serverRandValue);
@@ -366,7 +366,7 @@ void GameServer::DoTimedUpdates()
 	#endif
 
 	// Get the current time
-	time_t currentTime = time(0);
+	time_t currentTime = time(nullptr);
 	// Check for timed out game entries
 	for (std::size_t i = gameSessions.size(); i-- > 0; )
 	{
